9787817dc463184ec9b465f7a28ee6e0aa9dd760
egorka-hub <e.nuyckin@gmail.com>
2024-12-24 13:49:17 +0300

    feat(db): create bookings table

--- pg_db/init/01-schema.sql
+++ pg_db/init/01-schema.sql
@@ -69,6 +69,14 @@ CREATE TABLE IF NOT EXISTS rides (
     date_finished timestamp with time zone CHECK (date_finished > date_started)
 );
 
+CREATE TABLE IF NOT EXISTS bookings (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL REFERENCES users(id),
+    scooter_id uuid NOT NULL REFERENCES scooters(id),
+    date_started timestamp with time zone NOT NULL DEFAULT now(),
+    date_finished timestamp with time zone NOT NULL CHECK (date_finished > date_started)
+);
+
 CREATE TABLE IF NOT EXISTS parkings (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     location geography NOT NULL
