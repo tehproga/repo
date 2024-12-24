d648d41025473f3847673736acdf19abe8e78d1b
egorka-hub <e.nuyckin@gmail.com>
2024-12-24 13:27:50 +0300

    feat(db): create rides table

--- pg_db/init/01-schema.sql
+++ pg_db/init/01-schema.sql
@@ -46,6 +46,16 @@ CREATE TABLE IF NOT EXISTS scooters (
     number varchar(8) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS rides (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL REFERENCES users(id),
+    scooter_id uuid NOT NULL REFERENCES scooters(id),
+    start_price integer NOT NULL CHECK (start_price >= 0),
+    per_minute_price integer NOT NULL CHECK (per_minute_price >= 0),
+    date_started timestamp with time zone NOT NULL DEFAULT now(),
+    date_finished timestamp with time zone CHECK (date_finished > date_started)
+);
+
 CREATE TABLE IF NOT EXISTS restricted_zones (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     polygon geometry NOT NULL,
